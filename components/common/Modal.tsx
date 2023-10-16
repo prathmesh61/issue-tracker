07f2d6f436842edd9c4fe1f0513e723cd2b8afc6
prathmesh <prathmeshpol61@gmail.com>
2023-10-16 09:01:17 +0530

    small css changes

--- components/common/Modal.tsx
+++ components/common/Modal.tsx
@@ -19,12 +19,16 @@ const Modal = ({ issue }: Props) => {
   return (
     <>
       <div
-        className={`shadow-md cursor-pointer rounded-md flex flex-col gap-2 items-start w-fit ${bgPrority} font-bold capitalize text-sm px-6 py-3 hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150`}
+        className={`shadow-md cursor-pointer rounded-md flex flex-col  items-start w-fit ${bgPrority} font-bold capitalize text-sm px-6 py-3 hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150`}
         onClick={() => setShowModal(true)}
       >
         <h4 className="text-md font-bold whitespace-nowrap">{issue.title}</h4>
-        <h4 className="text-xs font-semibold">Email - {issue.email}</h4>
-        <h4 className="text-xs font-semibold">prority - {issue.order}</h4>
+        <h4 className="text-[10px] text-zinc-500 font-semibold">
+          Email - {issue.email}
+        </h4>
+        <h4 className="text-[10px] text-zinc-500 font-semibold">
+          prority - {issue.order}
+        </h4>
       </div>
       {showModal ? (
         <>
@@ -34,15 +38,15 @@ const Modal = ({ issue }: Props) => {
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex flex-col gap-3 items-start justify-between p-5 border-b border-solid  rounded-t">
-                  <h3 className="text-3xl font-semibold">{issue.title}</h3>
+                  <h3 className="text-2xl font-semibold">{issue.title}</h3>
                   <div className="flex items-center gap-3">
                     <h3
-                      className={`text-xs font-semibold ${bgPrority} px-2 py-1 cursor-pointer`}
+                      className={`text-xs font-semibold ${bgPrority} px-2 py-1 `}
                     >
                       Importance :- {issue.order}
                     </h3>
                     <h3
-                      className={`text-xs font-semibold bg-lime-200 px-2 py-1 cursor-pointer`}
+                      className={`text-xs font-semibold bg-lime-200 px-2 py-1 `}
                     >
                       Email :- {issue.email}
                     </h3>
@@ -57,7 +61,8 @@ const Modal = ({ issue }: Props) => {
                 <div className="relative p-6 flex-auto w-fit">
                   <Link
                     href={issue.link}
-                    className=" bg-purple-300 px-2 py-1  text-md w-fit"
+                    target="_blank"
+                    className=" bg-purple-300 px-2 py-1  text-xs w-fit"
                   >
                     Issue Link - {issue.link}
                   </Link>
