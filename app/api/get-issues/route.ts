97b1e451109af2ba10d6958959a75e45cd1ba28c
prathmesh <prathmeshpol61@gmail.com>
2024-01-09 08:05:47 +0530

    add - caching in api using node-cache

--- app/api/get-issues/route.ts
+++ app/api/get-issues/route.ts
@@ -1,9 +1,19 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import NodeCache from "node-cache";
 
+// node-cache init
+const nodeCache = new NodeCache();
 export const GET = async (req: NextRequest, res: NextResponse) => {
+  let issues;
   try {
-    const issues = await prisma.issue.findMany();
+    // node caching added for better UI Experience
+    if (nodeCache.has("issues")) {
+      issues = nodeCache.get("issues")!;
+    } else {
+      issues = await prisma.issue.findMany();
+      nodeCache.set("issues", issues);
+    }
     return NextResponse.json(issues);
   } catch (error) {
     return NextResponse.json(error, { status: 500 });
--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,7 +1,9 @@
 import prisma from "@/prisma/client";
 import { formSchema } from "@/utils/validation/formSchema";
 import { NextRequest, NextResponse } from "next/server";
+import NodeCache from "node-cache";
 
+const nodeCache = new NodeCache();
 type IssueBody = {
   title: string;
   description: string;
@@ -27,6 +29,8 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
         order: body.prority,
       },
     });
+    // cache delete for changes in UI
+    nodeCache.del("issues");
     return NextResponse.json(newIssue, { status: 201 });
   } catch (error) {
     return NextResponse.json(error, { status: 500 });
